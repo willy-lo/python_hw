759922ad9168310b1329d5671986d76a812f6097
willy-lo <47964093+willy-lo@users.noreply.github.com>
2019-02-26 16:13:08 +0800

    Add files via upload

---
+++ hw01.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 17 13:51:19 2019
+
+@author: lab819
+"""
+
+print("1.系級:電機所碩一 學號:107061597 姓名:羅偉誠\n")
+print("2.學過一點點matlab\n")
+print("3.想學習python這個語言並打好基礎\n")
+print("4.學到如何寫網路爬蟲,視覺化,及文字雲製作")
+
+
+
+
+
+
+ 
